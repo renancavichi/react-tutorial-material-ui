fed885e94427dafd16938f62ecaa0e9a9355ee7e
Renan Cavichi <renancavichi@gmail.com>
2023-03-09 18:09:01 -0300

    add icon alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,18 @@
-import { borderRadius, padding } from "@mui/system"
+import {MdOutlineDone, MdInfoOutline, MdOutlineWarningAmber} from 'react-icons/md'
+import {VscError} from 'react-icons/vsc'
 
 const typeColor = {
-    success: "#cdefd6",
+    success: '#cdefd6',
     error: '#efcdcd',
     info: '#cddeef',
     warning: '#ebe5b7'
+}
+
+const typeIcon = {
+    success: <MdOutlineDone style={{color: '#2f7441'}}/>,
+    error: <VscError style={{color: '#2f7441'}}/>,
+    info: <MdInfoOutline style={{color: '#244a70'}}/>,
+    warning: <MdOutlineWarningAmber style={{color: '#244a70'}}/>
 }
 
 const Alert = ({type, children}) => {
@@ -12,16 +20,18 @@ const Alert = ({type, children}) => {
     const bg = {
         background: typeColor[type],
         padding: '20px',
-        borderRadius: '10px'
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '10px'
     }    
     
     return (
         <div style={bg}>
-            {children}
+            {typeIcon[type]}
+            <span>{children}</span>
         </div>
     )
 }
-
-
 
 export default Alert
