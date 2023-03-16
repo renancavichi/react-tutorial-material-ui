be2282a37547af33ad03750a57f167f076cb5051
Renan Cavichi <renancavichi@gmail.com>
2023-03-16 17:43:31 -0300

    sample of usestate

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box'
 import Alert from './Alert'
+import SampleUseState from './SampleUseState'
 
 const Content = () => {
 
@@ -7,7 +8,6 @@ const Content = () => {
         <Box sx={{
             background: "#EEE",
             width: 500,
-            height: 500,
             padding: '15px'
         }}>
             <h1>Content</h1>
@@ -15,18 +15,20 @@ const Content = () => {
                 <Alert type="success" >
                     Deu certo!
                 </Alert>
-
+                <br />
                 <Alert type="error">
                     Deu Erro!
                 </Alert>
-
+                <br />
                 <Alert type="info">
                     Info!
                 </Alert>
-
+                <br />
                 <Alert type="warning">
                    Warning!
-                </Alert>   
+                </Alert> 
+
+                <SampleUseState />
             
         </Box>
     )
---
+++ src/components/SampleUseState.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const SampleUseState = () => {
+
+  const [counter, setCounter] = useState(0)
+  const [name, setName] =  useState('Fulano')
+
+  console.log('Component log...')
+ 
+  return (
+    <div>
+        <h2>Contador: {counter}</h2>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <button onClick={() => setCounter(counter-1)}>-</button>
+        <hr />
+
+        <h2>Olá {name}</h2>
+        <button onClick={() => setName('Renan')}>Renan</button>
+        <button onClick={() => setName('Pedro')}>Pedro</button>
+    </div>
+  )
+}
+
+export default SampleUseState
