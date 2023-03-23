15cad72e509e95d021ca10ca14b8c8b77244fa43
Renan Cavichi <renancavichi@gmail.com>
2023-03-23 17:15:42 -0300

    create app router

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Error404 from './pages/Error404'
+
+const Router = () => {
+    return( 
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quem-somos" element={<About />} />
+            <Route path="*" element={<Error404 />} />
+        </Routes>
+    )
+}
+
+export default Router
