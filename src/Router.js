da02bb7abb97f8244caf8f69306fadab667022d8
Renan Cavichi <renancavichi@gmail.com>
2023-03-23 17:58:56 -0300

    add route contact

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Error404 from './pages/Error404'
+import Contact from './pages/Contact'
 
 const Router = () => {
     return( 
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/quem-somos" element={<About />} />
+            <Route path="/contato" element={<Contact />} />
             <Route path="*" element={<Error404 />} />
         </Routes>
     )
