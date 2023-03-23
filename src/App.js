009588e9728f52cb2ddc64349f9d608b631cbe67
Renan Cavichi <renancavichi@gmail.com>
2023-03-23 17:15:26 -0300

    add router component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import Box from '@mui/material/Box'
-
-import Header from './components/Header'
-import MainMenu from './components/MainMenu'
-import Sidebar from './components/Sidebar'
-import Content from './components/Content'
+import Router from './Router'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <MainMenu />
-      <Box sx={{
-        display: 'flex',
-      }}>
-        <Sidebar />
-        <Content />
-      </Box>
-      {/* <Footer /> */}
-    </>
+    <Router />
   )
 }
 
