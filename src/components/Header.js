fd6eab200bb9b15a0c364d18a975a8dcd9cb0d1f
Renan Cavichi <renancavichi@gmail.com>
2023-03-23 17:59:36 -0300

    add logo react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import './Header.css'
 import Box from '@mui/material/Box'
+import {FaReact as IconReact} from 'react-icons/fa'
 
 const Header = () => {
     return (
         <Box component='header'>
-            <h3>Logo</h3>
+            <Box sx={styles.stack}>
+                <IconReact style={styles.logo} />
+                <h3>Logo</h3>
+            </Box>
         </Box>
     )
+}
+
+const styles = {
+    stack: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItens: 'center',
+        gap: '10px',
+        width: 'fit-content'
+    },
+    logo: {
+        color: '#FFF',
+        width: '40px',
+        height: '40px'
+    }
 }
 
 export default Header
