8c84fa1836e006d03417197a1de3ffba1e454cc2
Renan Cavichi <renancavichi@gmail.com>
2023-04-14 16:03:12 -0300

    add main menu links

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,22 +1,41 @@
 import Box from '@mui/material/Box'
+import { NavLink } from 'react-router-dom'
 
 const MainMenu = () => { 
     return (
         <Box component="nav" sx={styles.nav}>
-            <ul style={styles.ulMenu}>menu...</ul>
+            <ul style={styles.ulMenu}>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/quem-somos'>Quem Somos</NavLink></li>
+                <li><NavLink to='/users'>Users</NavLink></li>
+                <li><NavLink to='/contato'>Contato</NavLink></li>
+            </ul>
         </Box>
     )
 }
 
 const styles = {
     nav: {
-        background: '#555',
-        color: '#000',
-        padding: '20px',
-        marginTop: '5px'
+        color: '#FFF',
+        margin: '0 0 0 auto',
+        '& ul li a': {
+            color: '#FFF',
+            textDecoration: 'none',
+            padding: '0 5px 8px 5px',
+            '&:hover': {
+                color: '#DDD',
+                borderBottom: '3px solid #f0c14b',
+            }
+        }
     },
     ulMenu: {
-        listStyleType: 'none'
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        gap: '10px',
+        listStyleType: 'none',
+        padding: '0',
+        margin: '0'
     }
 }
 
