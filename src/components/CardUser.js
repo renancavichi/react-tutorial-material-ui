6e2df3e9f9c294eb3bb3c627c040a9e282eb5a08
Renan Cavichi <renancavichi@gmail.com>
2023-04-28 17:57:01 -0300

    update style card user

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -2,11 +2,11 @@ import Box from '@mui/material/Box'
 
 const CardUser = ({user}) => {
   return (
-    <Box sx={{background: '#D3D3D3', borderRadius: '10px', display: 'flex', margin: '20px 0', padding: '15px'}}>
+    <Box sx={{background: '#e7e7e7', borderRadius: '10px', display: 'flex', gap: '15px', margin: '20px 0', padding: '15px'}}>
         <img src={user.avatar} alt={user.name} style={{width: '80px',height: '80px', borderRadius: '50%'}}/>
         <Box>
-            <h3>{user.name}</h3>
-            <p>{user.email}</p>
+            <h3 style={{margin: '10px 0 5px 0'}}>{user.name}</h3>
+            <p style={{margin: '0'}}>{user.email}</p>
         </Box>
     </Box>
   )
